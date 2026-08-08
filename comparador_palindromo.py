66b4454a355c3f5da3b164cfa2046cf107bb537f
AlexDcZero <alejandro.duran94@hotmail.com>
2026-08-08 03:16:59 +0200

    Actualizada la salida de info

--- comparador_palindromo.py
+++ comparador_palindromo.py
@@ -1,7 +1,8 @@
-def es_palindromo(texto):
+def es_palindromo():
+    texto = input("Introduzca una cadena:")
     # 1. Limpieza de datos
     texto_limpio = texto.lower().replace(" ", "")
-    
+
     # 2. Inicializar punteros
     inicio = 0
     fin = len(texto_limpio) - 1
@@ -9,16 +10,15 @@ def es_palindromo(texto):
     # 3. Comparación desde los extremos al centro
     while inicio < fin:
         if texto_limpio[inicio] != texto_limpio[fin]:
+            print(f"La cadena {texto} no es un palindromo")
             return False  # Si no coinciden, no es palíndromo
-        
+
         inicio = inicio + 1  # Avanzar hacia la derecha
         fin = fin - 1        # Retroceder hacia la izquierda
         
     # 4. Si el bucle termina, ¡es un palíndromo!
+    print(f"La cadena {texto} es un palindromo!")
     return True
 
 # --- Pruebas ---
-print(es_palindromo("Anita lava la tina"))  # Salida: True
-print(es_palindromo("0110"))                # Salida: True
-print(es_palindromo("Python"))               # Salida: False
-print(es_palindromo("0000"))
+es_palindromo()
